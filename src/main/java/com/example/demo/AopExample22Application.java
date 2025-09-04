313c5c153782b23bf74a20654cb2878f47423a09
Vedantmodi39 <modivedant24@gmail.com>
2025-09-04 16:33:16 +0530

    make it runnable

--- src/main/java/com/example/demo/AopExample22Application.java
+++ src/main/java/com/example/demo/AopExample22Application.java
@@ -5,19 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
+@SpringBootApplication
 public class AopExample22Application {
 
 	public static void main(String[] args) {
-		//SpringApplication.run(AopExample22Application.class, args);
-		 
-		
-		ApplicationContext context= new  AnnotationConfigApplicationContext(BeanConfig.class);
-		// aa context mathi bean get karai skiae...	 
-		
-		ShoppingCart cart= context.getBean(ShoppingCart.class);
-		cart.Checkout("cancelled");
-		cart.quantity();
+		SpringApplication.run(AopExample22Application.class, args);
+		System.out.println("--------Application Started-------");
 	}
 
 	
